ed38c8359f2565f6ea5e581d3219231973e3f278
Taylor Jean <taylorgjean@gmail.com>
2022-09-20 22:42:29 +0200

    organize api

--- server.js
+++ server.js
@@ -10,21 +10,31 @@ const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const portfolioRoutes = require('./routes/portfolio')
 const axios = require('axios')
+
+//Use .env file in config folder
 require('dotenv').config({path: './config/.env'})
 
 // Passport config
 require('./config/passport')(passport)
 
+//Connect To Database
 connectDB()
 
+//Using EJS for views
 app.set('view engine', 'ejs')
+
+//Static Folder
 app.use(express.static('public'))
+
+//Body Parsing
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+//Logging
 app.use(logger('dev'))
 
 
-// Sessions
+// Setup Sessions - stored in MongoDB
 app.use(
     session({
       secret: 'keyboard cat',
@@ -37,14 +47,16 @@ app.use(
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+//Use flash messages for errors, info, etc.
 app.use(flash())
 
 
-//ROUTES
+//Setup Routes For Which The Server Is Listening
 app.use('/', mainRoutes)
 app.use('/portfolio', portfolioRoutes)
 
-//LISTEN
+//Server Running
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it on !')
 })    
