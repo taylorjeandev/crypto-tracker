d8562858ea6ba9cc49dce074e54e1393cad960d0
Taylor Jean <taylorgjean@gmail.com>
2022-10-19 13:05:38 +0200

    adjusted .env

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const portfolioRoutes = require('./routes/portfolio');
 const axios = require('axios');
 
 //Use .env file in config folder
-require('dotenv').config({path: './.env'});
+require('dotenv').config({path: './config/.env'});
 
 // Passport config
 require('./config/passport')(passport)
