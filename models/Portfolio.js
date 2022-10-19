71edd1860bc4ad3375a8daefcfd3d2d79eaa9db0
Taylor Jean <taylorgjean@gmail.com>
2022-10-19 14:16:05 +0200

    add Portfolio.js

---
+++ models/Portfolio.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const PortfolioSchema = new mongoose.Schema({
+  coin: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  pricePaid:{
+    type: Number,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model('Portfolio', PortfolioSchema)
