491a927dfe4068dfafaaf3be44218a2d90d8f514
Taylor Jean <taylorgjean@gmail.com>
2022-10-19 13:46:20 +0200

    restored package-lock

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+const Portfolio = require('../models/Portfolio')
+const User = require('../models/User')
 
 module.exports = {
     getIndex: async (req, res) => {
@@ -6,7 +8,7 @@ module.exports = {
 
             const data = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=15&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C1y')
     
-            res.render('index.ejs', {currencies: data.data,user:req.user})
+            res.render('index.ejs', {currencies: data.data, user:req.user})
 
         } catch (err) {
             console.error(err)
--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -1,4 +1,4 @@
-const Portfolio = require('../models/Portfolio.js')
+const Portfolio = require('../models/Portfolio')
 const axios = require("axios");
 
 module.exports = {
