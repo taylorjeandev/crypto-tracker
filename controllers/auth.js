415608072db97f25e405c39234870400bd41ac51
taylorjeandev <taylorgjean@gmail.com>
2022-11-08 18:32:43 +0100

    update passport logout function and remove console.log on rest api

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,16 +44,20 @@ const User = require('../models/User')
     })(req, res, next)
   }
 
-  // LOGIN OUT OF THE APPLICATION
+  // LOGOUT OF THE APPLICATION
 
   exports.logout = (req, res) => {
-    req.logout()
-    req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
-      req.user = null
-      res.redirect('/')
+    req.logout(() => {
+      console.log('User has logged out.')
     })
-  }
+    req.session.destroy((err) => {
+      if (err)
+        console.log("Error : Failed to destroy the session during logout.", err);
+      req.user = null;
+      res.redirect("/");
+    });
+  };
+  
 
   //SIGNING UP INTO THE APP
 
