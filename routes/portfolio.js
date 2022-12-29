25e75df13382ba073af41b6e776290dcf9a4355c
taylorjeandev <taylorgjean@gmail.com>
2022-12-29 21:29:00 +0100

    format files

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,16 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const portfolioController = require('../controllers/portfolio') 
-const { ensureAuth } = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const portfolioController = require("../controllers/portfolio");
+const { ensureAuth } = require("../middleware/auth");
 
-router.get('/', ensureAuth, portfolioController.getPortfolio)
+router.get("/", ensureAuth, portfolioController.getPortfolio);
 
-router.post('/createCoin', portfolioController.createCoin)
+router.post("/createCoin", portfolioController.createCoin);
 
-router.put('/updateCoin', portfolioController.updateCoin)
+router.put("/updateCoin", portfolioController.updateCoin);
 
-router.delete('/deleteCoin', portfolioController.deleteCoin)
+router.delete("/deleteCoin", portfolioController.deleteCoin);
 
-
-
-module.exports = router
+module.exports = router;
